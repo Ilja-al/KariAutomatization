8df104d631c61cea53f696af038f65d062ba2f5f
Ilja_al <gusenkov.ilja@yandex.ru>
2025-01-22 16:32:45 +0500

    allure-feature and headless

--- test/conftest.py
+++ test/conftest.py
@@ -4,10 +4,18 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def browser():
-    #options = Options()
-    #options.add_argument('--headless') # опция для запуска в безголовом режиме
-    browser = webdriver.Chrome()#(options=options) # открываем Chrome
-    browser.maximize_window() # открываем на полную
-    #browser.implicitly_wait(5) # неявное ожидание 5 секунд
-    yield browser
-    browser.quit()
+    options = Options()
+    options.add_argument('--headless')  # Безголовый режим
+    options.add_argument('--disable-gpu')  # Отключение GPU
+    options.add_argument('--no-sandbox')  # Полезно для Docker
+    options.add_argument('--window-size=1920,1080')  # Размер окна
+
+    browser = webdriver.Chrome(options=options)  # Запуск браузера
+
+    #browser.maximize_window()  # Разворачивание на полный экран
+    #browser.implicitly_wait(5)  # Неявное ожидание 5 секунд
+
+    try:
+        yield browser
+    finally:
+        browser.quit()  # Гарантированное закрытие браузера
--- test/test_im_reg_auth.py
+++ test/test_im_reg_auth.py
@@ -18,13 +18,6 @@ def test_reg_form_check(browser):
     assert main_page_auth_reg.reg_check().is_displayed()
 
 @allure.feature('ИМ - Регистрация/авторизация')
-@allure.story('Регистрация. Переход к форме заполнения данных нового аккаунта"')
-def test_reg_form_input_tel(browser):
-    auth_reg = RegPage(browser)
-    auth_reg.open_reg_page()
-    assert auth_reg.reg_input_tel().is_displayed()
-
-@allure.feature('ИМ - Регистрация/авторизация')
 @allure.story('Регистрация. Переход к форме заполнения данных нового аккаунта')
 @allure.title('Ввести в поле ввода номера символы и буквы')
 def test_reg_form_input_tel_symbol(browser):
@@ -33,24 +26,35 @@ def test_reg_form_input_tel_symbol(browser):
     auth_reg.reg_input_tel_symbol()
     assert auth_reg.reg_input_tel_required().is_displayed()
 
-@allure.feature('Registration. Go to the form new account data')
-@allure.story('Click button "Create acc"')
+@allure.feature('ИМ - Регистрация/авторизация')
+@allure.story('Регистрация. Переход к форме заполнения данных нового аккаунта')
+@allure.title('Ввести в поле ввода номера недостаточное кол-во символов')
 def test_reg_from_input_tel_symbol_limit(browser):
     auth_reg = RegPage(browser)
     auth_reg.open_reg_page()
     auth_reg.reg_input_tel_symbol_limit()
     assert auth_reg.reg_input_tel_part_empty().is_displayed()
 
-@allure.feature('Registration. Go to the form new account data')
-@allure.story('Input_unregistered_tel')
+@allure.feature('ИМ - Регистрация/авторизация')
+@allure.story('Регистрация. Переход к форме заполнения данных нового аккаунта')
+@allure.title('Ввести номер телефона незарегестрированного пользователя и нажать кнопку "Получить код"')
 def test_input_unregistered_tel(browser):
     auth_reg = RegPage(browser)
     auth_reg.open_reg_page()
     auth_reg.input_unregistered_tel()
     assert auth_reg.find_captcha().is_displayed()
 
-@allure.feature('Authorization')
-@allure.story('Auth. Validation login field')
+@allure.feature('ИМ - Регистрация/авторизация')
+@allure.story('Регистрация. Переход к форме заполнения данных нового аккаунта"')
+@allure.title('Ввести номер телефона зарегестрированного пользователя и нажать кнопку "Получить код"')
+def test_reg_form_input_tel(browser):
+    auth_reg = RegPage(browser)
+    auth_reg.open_reg_page()
+    assert auth_reg.reg_input_tel().is_displayed()
+
+@allure.feature('ИМ - Регистрация/авторизация')
+@allure.story('Авторизация. Валидация поля Телефон/E-mail')
+@allure.title('Ввод различных данных в поле логина и проверка валидации')
 @pytest.mark.parametrize("email, expected_error", [
         ('test@example.com',''),
         ('79022779866',''),
@@ -65,8 +69,9 @@ def test_auth_valid_login_field(browser, email, expected_error):
     auth_page.valid_login_field(email, expected_error)
 
 
-@allure.feature('Authorization')
-@allure.story('Auth. Check auth')
+@allure.feature('ИМ - Регистрация/авторизация')
+@allure.story('Авторизация. Проверка авторизации.')
+@allure.title('Ввод различных данных и проверка авторизации ')
 @pytest.mark.parametrize("login, password, error", [
         ('79112223388', 'PolkA890!', 'Неверный логин или пароль'),
         ('79000000000', 'PolkA890!', 'Неверный логин или пароль'),
@@ -77,15 +82,18 @@ def test_auth_check_auth(browser, login, password, error):
     auth_page.open_auth_page()
     auth_page.check_auth_check(login, password, error)
 
-@allure.feature('Authorization')
-@allure.story('Auth. Switch between login methods')
+
+@allure.feature('ИМ - Регистрация/авторизация')
+@allure.story('Авторизация. Проверка переключения между способами входа.')
+@allure.title('Авторизация. Проверка переключения между способами входа.')
 def test_switch_between_login_methods(browser):
     auth_page = AuthPage(browser)
     auth_page.open_auth_page()
     auth_page.switch_between_login_methods()
 
-@allure.feature('Authorization')
-@allure.story('Auth. Ya VK displayed')
+@allure.feature('ИМ - Регистрация/авторизация')
+@allure.story('Yandex и ВК ID. Отображение кнопок входа')
+@allure.title('Yandex и ВК ID. Отображение кнопок входа')
 def test_ya_vk_displayed(browser):
     auth_page = AuthPage(browser)
     auth_page.open_auth_page()
