e8165606fa5521d96a6a575659e98ae6b1d23dce
Ilja_al <gusenkov.ilja@yandex.ru>
2025-01-31 18:24:28 +0500

    config

--- config/links.py
+++ config/links.py
@@ -1,5 +1,5 @@
 class Links:
-    HOST = 'https://test-not-prod.kari.com'
-    AUTH_PAGE = f'{HOST}/auth/'
-    REG_PAGE = f'{HOST}/auth/reg/'
-    RECOVERY_PAGE = f'{HOST}/auth/recovery/'
+    HOST = 'https://test-not-prod.kari.com/'
+    AUTH_PAGE = f'{HOST}auth/'
+    REG_PAGE = f'{HOST}auth/reg/'
+    RECOVERY_PAGE = f'{HOST}auth/recovery/'
