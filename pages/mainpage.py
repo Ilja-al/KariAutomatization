7b314c436643fe11c804a5e0fd92b49dcc04c999
Ilja_al <145164212+Ilja-al@users.noreply.github.com>
2025-02-02 14:18:54 +0500

    Update mainpage.py

--- pages/mainpage.py
+++ pages/mainpage.py
@@ -2,60 +2,59 @@ from selenium.webdriver.common.by import By
 import allure
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from base.base_page import BasePage
-from config.links import Links
 
 
-class MainPage(BasePage):
+class MainPage:
 
-    # Метод для ожидания наличия элемента (работает с CSS и XPath)
-    def wait_for_element(self, locator, by=By.XPATH, timeout=60):
-            return WebDriverWait(self.browser, timeout).until(
-                EC.presence_of_element_located((by, locator))
+    # Локаторы элементов
+    LOGIN_ICON_LOCATOR = '//button[@class="css-1svjifm e2cllgv3"]'  # Иконка входа
+    ENTER_REGISTRATION_BUTTON_LOCATOR = '//a[@class="css-oc613h e1bfq77c24"]'  # Кнопка "Войти или зарегистрироваться"
+
+    def __init__(self, browser):
+        self.browser = browser
+
+    # Метод для открытия главной страницы
+    def open_main_page(self, url):
+        with allure.step('Открыть главную страницу'):
+            print(f"Попытка открыть сайт: {url}")
+            self.browser.get(url)
+            # Проверяем, что загруженный URL совпадает с ожидаемым
+            WebDriverWait(self.browser, 30).until(
+                EC.url_to_be(url)
             )
+            if url != self.browser.current_url:
+                raise AssertionError(f"Ошибка: Ожидался URL: {url}, но фактический URL: {self.browser.current_url}")
+            else:
+                print(f"Сайт успешно открыт: {self.browser.current_url}")
 
-    # Метод для ожидания кликабельности элемента
-    def wait_for_clickable_element(self, locator, by=By.XPATH, timeout=60):
-            return WebDriverWait(self.browser, timeout).until(
-                EC.element_to_be_clickable((by, locator))
-            )
+    # Метод для нажатия на иконку входа
+    def click_login_icon(self):
+        with allure.step('Нажать иконку входа'):
+            login_icon = self.wait_for_clickable_element(MainPage.LOGIN_ICON_LOCATOR)
+            login_icon.click()
 
-    # Метод для проверки видимости элемента
-    def check_element_displayed(self, locator, by=By.XPATH):
-        element = self.wait_for_element(locator, by)
+    # Метод для нажатия на кнопку "Войти или зарегистрироваться"
+    def click_enter_registration(self):
+        with allure.step('Нажать "Войти или зарегистрироваться"'):
+            enter_registration = self.wait_for_clickable_element(MainPage.ENTER_REGISTRATION_BUTTON_LOCATOR)
+            enter_registration.click()
+
+    # Общий метод для ожидания и проверки видимости элемента
+    def check_and_wait_for_element(self, locator, by=By.XPATH, timeout=60):
+        element = WebDriverWait(self.browser, timeout).until(
+            EC.visibility_of_element_located((by, locator))
+        )
         assert element.is_displayed(), f"Элемент {locator} не отображается"
         return element
 
-    def open_main_page(self):
-        with allure.step('Открыть сайт'):
-            page_url = Links.HOST
-            print(f"Попытка открыть сайт: {page_url}")  # Выводим, какой URL пытаемся открыть
-            self.browser.get(page_url)
-            # Печатаем текущий URL после загрузки страницы
-            print(f"Текущий URL: {self.browser.current_url}")
-            # Ожидаем, что в URL будет содержаться `page_url`
-            WebDriverWait(self.browser, 30).until(
-                EC.url_contains(page_url)
-            )
-            # Печатаем, если сайт все-таки не открылся как нужно
-            if page_url not in self.browser.current_url:
-                print(f"Ошибка: Ожидался URL: {page_url}, но фактический URL: {self.browser.current_url}")
-            assert page_url in self.browser.current_url, 'Не удалось открыть сайт'
-            #print("Ожидание загрузки кнопки")
-            #button_submit = self.wait_for_clickable_element('//button[text()="Применить"]')
-            #print("Элемент найден, кликаем по кнопке...")
-            #button_submit.click()
+    # Метод для ожидания наличия элемента (работает с CSS и XPath)
+    def wait_for_element(self, locator, by=By.XPATH, timeout=60):
+        return WebDriverWait(self.browser, timeout).until(
+            EC.presence_of_element_located((by, locator))
+        )
 
-    def click_login_icon(self): # Иконка входа
-        with allure.step('Нажать иконку входа'):
-            print("Поиск иконки")    
-            login_icon = self.wait_for_clickable_element('//button[@class="css-1svjifm e2cllgv3"]')
-            print("Нажатие на иконку")    
-            login_icon.click()
-
-    def click_enter_registration(self): # войти или зарегистрироваться
-        with allure.step('Нажать "Войти или зарегистрироваться"'):
-            print("Поиск кнопки Войти")     
-            enter_registration = self.wait_for_clickable_element('//a[@class="css-oc613h e1bfq77c24"]')
-            print("Нажатие кнопки Войти")  
-            enter_registration.click()
+    # Метод для ожидания кликабельности элемента
+    def wait_for_clickable_element(self, locator, by=By.XPATH, timeout=60):
+        return WebDriverWait(self.browser, timeout).until(
+            EC.element_to_be_clickable((by, locator))
+        )
